c8f93b870682754b41fdf32653618bf6686b947f
Mincong Huang <mhuang@becomposable.com>
2024-12-21 15:53:43 +0100

    Test workflows

--- packages/github-agent/src/workflows.test.ts
+++ packages/github-agent/src/workflows.test.ts
@@ -1,19 +1,72 @@
-// import { TestWorkflowEnvironment } from '@temporalio/testing';
+import { TestWorkflowEnvironment } from '@temporalio/testing';
+import { Worker } from '@temporalio/worker';
+import {
+    afterAll,
+    beforeAll,
+    expect,
+    test
+} from 'vitest';
+import type * as activities from './activities';
+import { GetMultiRepoInfoResponse, getRepos } from './workflows';
 
-// let testEnv: TestWorkflowEnvironment;
 
-// // beforeAll and afterAll are injected by Jest
-// beforeAll(async () => {
-//   testEnv = await TestWorkflowEnvironment.createTimeSkipping();
-// });
+let testEnv: TestWorkflowEnvironment;
+let worker: Worker;
 
-// afterAll(async () => {
-//   await testEnv?.teardown();
-// });
+beforeAll(async () => {
+    const mockActivities: Partial<typeof activities> = {
+        getRepoInfo: async (request: activities.GetRepoInfoRequest): Promise<activities.GetRepoInfoResponse> => {
+            const [owner, repo] = request.name.split('/');
+            return {
+                owner,
+                repo,
+                repo_url: `https://github.com/${owner}/${repo}`,
+                description: '',
+            };
+        },
+    };
 
-import { describe, test } from 'vitest'
+    testEnv = await TestWorkflowEnvironment.createTimeSkipping();
+    worker = await Worker.create({
+        connection: testEnv.nativeConnection,
+        taskQueue: 'test',
+        activities: mockActivities,
+        workflowsPath: require.resolve('./workflows.ts'),
+    });
+});
 
-describe('Workflow Test Suite', () => {
-  test('Test 1', async () => { /* ... */ })
-  test('Test 2', async () => { /* ... */ })
-})
+afterAll(async () => {
+    await testEnv?.teardown();
+});
+
+test('workflow: getRepos', async () => {
+    const result = await worker.runUntil(
+        testEnv.client.workflow.execute(getRepos, {
+            taskQueue: 'test',
+            workflowId: 'test',
+            args: [{
+                names: [
+                    'mincong-h/mincong-h.github.io',
+                    'mincong-h/learning-node'
+                ]
+            }],
+        })
+    );
+    expect(result).toEqual({
+        count: 2,
+        repos: [
+            {
+                owner: 'mincong-h',
+                repo: 'mincong-h.github.io',
+                repo_url: 'https://github.com/mincong-h/mincong-h.github.io',
+                description: '',
+            },
+            {
+                owner: 'mincong-h',
+                repo: 'learning-node',
+                repo_url: 'https://github.com/mincong-h/learning-node',
+                description: '',
+            },
+        ]
+    } as GetMultiRepoInfoResponse);
+});
