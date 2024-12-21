8b670f6cfcb2dff9c9849c0e7fde0bdd23699f1c
Mincong Huang <mhuang@becomposable.com>
2024-12-21 15:10:18 +0100

    Set up tests

--- packages/github-agent/src/workflows.test.ts
+++ packages/github-agent/src/workflows.test.ts
@@ -1,12 +1,19 @@
-import { TestWorkflowEnvironment } from '@temporalio/testing';
+// import { TestWorkflowEnvironment } from '@temporalio/testing';
 
-let testEnv: TestWorkflowEnvironment;
+// let testEnv: TestWorkflowEnvironment;
 
-// beforeAll and afterAll are injected by Jest
-beforeAll(async () => {
-  testEnv = await TestWorkflowEnvironment.createTimeSkipping();
-});
+// // beforeAll and afterAll are injected by Jest
+// beforeAll(async () => {
+//   testEnv = await TestWorkflowEnvironment.createTimeSkipping();
+// });
 
-afterAll(async () => {
-  await testEnv?.teardown();
-});
+// afterAll(async () => {
+//   await testEnv?.teardown();
+// });
+
+import { describe, test } from 'vitest'
+
+describe('Workflow Test Suite', () => {
+  test('Test 1', async () => { /* ... */ })
+  test('Test 2', async () => { /* ... */ })
+})
