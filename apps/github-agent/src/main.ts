165ad3303e1c6375a539b7a831111586f683f480
Mincong Huang <mhuang@becomposable.com>
2024-12-27 16:16:47 +0100

    Add main.ts

---
+++ apps/github-agent/src/main.ts
@@ -0,0 +1,11 @@
+import { run, WorkerRunOptions, resolveScriptFile } from "@dglabs/agent-runner";
+
+const options: WorkerRunOptions = {};
+options.activities = await import('@dglabs/github-agent/activities')
+options.workflowBundle = await resolveScriptFile("@dglabs/github-agent/workflows/workflows-bundle", import.meta.url)
+
+await run(options).catch((err: any) => {
+    console.error(err);
+}).finally(() => {
+    process.exit(0);
+});
