8a1f03f0ab44c1d37e548887e73120f77b4b7619
Mincong Huang <mhuang@becomposable.com>
2024-12-30 11:55:23 +0100

    Bump dependency

--- apps/github-agent/src/main.ts
+++ apps/github-agent/src/main.ts
@@ -1,8 +1,11 @@
 import { run, WorkerRunOptions, resolveScriptFile } from "@dglabs/agent-runner";
 
-const options: WorkerRunOptions = {};
 // options.activities = await import('@dglabs/github-agent/activities')
-options.workflowBundle = await resolveScriptFile("@dglabs/github-agent/workflows/workflows-bundle", import.meta.url)
+const workflowBundle = await resolveScriptFile("@dglabs/github-agent/workflows/workflows-bundle", import.meta.url)
+
+const options: WorkerRunOptions = {
+    workflowBundle,
+};
 
 await run(options).catch((err: any) => {
     console.error(err);
