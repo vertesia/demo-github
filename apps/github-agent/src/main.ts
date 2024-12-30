eb2a4dab88f3498b0758340fd9d7e57c755e1187
Mincong Huang <mhuang@becomposable.com>
2024-12-30 12:10:54 +0100

    Add activities

--- apps/github-agent/src/main.ts
+++ apps/github-agent/src/main.ts
@@ -1,10 +1,11 @@
 import { run, WorkerRunOptions, resolveScriptFile } from "@dglabs/agent-runner";
 
-// options.activities = await import('@dglabs/github-agent/activities')
+const activities = await import('./activities')
 const workflowBundle = await resolveScriptFile("@dglabs/github-agent/workflows/workflows-bundle", import.meta.url)
 
 const options: WorkerRunOptions = {
     workflowBundle,
+    activities,
 };
 
 await run(options).catch((err: any) => {
