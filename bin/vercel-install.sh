daa28c12da633290fcbf748de198cc2deaab0314
Mincong Huang <mhuang@vertesiahq.com>
2025-02-04 22:32:38 +0100

    Don't install with turbo
    because the private packages Agent Runner SDK are lost in this case

--- bin/vercel-install.sh
+++ bin/vercel-install.sh
@@ -4,7 +4,5 @@ echo "[install] the current directory is \"$(pwd)\""
 npm -g i @vertesia/cli
 vertesia --version
 pnpm vertesia:connect
-turbo prune --scope=@dglabs/github-server
-cd out || exit
 pnpm install
 echo '[install] custom install command completed.'
