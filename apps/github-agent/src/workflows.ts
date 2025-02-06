b62f7669a081a8f88cf7705960126f29734ea6f2
Mincong Huang <mhuang@vertesiahq.com>
2025-02-06 07:16:09 +0100

    Add new workflow (#42)

--- apps/github-agent/src/workflows.ts
+++ apps/github-agent/src/workflows.ts
@@ -1,8 +1,6 @@
 import { log, proxyActivities } from "@temporalio/workflow";
 import * as activities from "./activities.js";
 
-// Export your workflow functions here
-
 const {
     helloActivity,
 } = proxyActivities<typeof activities>({
@@ -19,4 +17,19 @@ const {
 export async function helloWorkflow() {
     log.info("Entering Hello workflow");
     await helloActivity();
+}
+
+export type ReviewPullRequestRequest = {
+    /**
+     * The event that triggered this workflow in the GitHub API.
+     *
+     * See https://docs.github.com/en/webhooks/webhook-events-and-payloads#pull_request
+     */
+    githubEvent: any;
+}
+export type ReviewPullRequestResponse = {
+}
+export async function reviewPullRequest(request: ReviewPullRequestRequest): Promise<ReviewPullRequestResponse> {
+    log.info("Entering reviewPullRequest workflow:", request);
+    return {};
 }
