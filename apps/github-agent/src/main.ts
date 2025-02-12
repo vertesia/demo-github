73cfc75dce7a2eb48b7153b51c3e50cda5f3858e
Mincong Huang <mhuang@vertesiahq.com>
2025-02-12 16:57:11 +0100

    Hard-code the domain (task queue)

--- apps/github-agent/src/main.ts
+++ apps/github-agent/src/main.ts
@@ -1,20 +1,6 @@
-/**
- * This is a generated file. Do not modify.
- */
 import { resolveScriptFile, run } from "@dglabs/agent-runner";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from 'path';
 
-const pkg = readPackageJson();
-
-let domain = `agents/${pkg.name}`;
-if (pkg.vertesia?.image?.organization) {
-    if (pkg.vertesia.image.name) {
-        domain = `agents/${pkg.vertesia.image.organization}/${pkg.vertesia.image.name}`;
-    }
-}
-
+const domain = "agents/vertesia/github-agent";
 const workflowBundle = await resolveScriptFile("./workflows-bundle.js", import.meta.url);
 const activities = await import("./activities.js");
 
@@ -27,11 +13,3 @@ await run({
 }).finally(() => {
     process.exit(0);
 });
-
-
-function readPackageJson() {
-    const scriptPath = fileURLToPath(import.meta.url);
-    const pkgFile = resolve(dirname(scriptPath), '../package.json')
-    const content = readFileSync(pkgFile, "utf8");
-    return JSON.parse(content);
-}
