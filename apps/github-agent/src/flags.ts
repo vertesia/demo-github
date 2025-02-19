08f5ffed994e8c41985b4a6628490ac29f9db20f
Mincong Huang <mhuang@vertesiahq.com>
2025-02-19 14:48:22 +0100

    Add flag

--- apps/github-agent/src/flags.ts
+++ apps/github-agent/src/flags.ts
@@ -1,18 +1,22 @@
 export type UserFeatures = {
+    isCodeReviewEnabled: boolean;
     isDeploymentSummaryEnabled: boolean;
     isDiffSummaryEnabled: boolean;
 }
 
 const enabledUsers: Record<string, UserFeatures> = {
     'antoine-regnier': {
+        isCodeReviewEnabled: true,
         isDeploymentSummaryEnabled: true,
         isDiffSummaryEnabled: true,
     },
     'mincong-h': {
+        isCodeReviewEnabled: true,
         isDeploymentSummaryEnabled: true,
         isDiffSummaryEnabled: true,
     },
     'yangchi-tw': {
+        isCodeReviewEnabled: true,
         isDeploymentSummaryEnabled: true,
         isDiffSummaryEnabled: true,
     },
