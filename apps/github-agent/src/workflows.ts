2cac288f3c3e15483e1e9581de597dc543551639
Mincong Huang <mhuang@vertesiahq.com>
2025-02-25 10:09:19 +0100

    Only update vertesia comment if the vercel URL is undefined (#90)

--- apps/github-agent/src/workflows.ts
+++ apps/github-agent/src/workflows.ts
@@ -387,25 +387,22 @@ async function handlePullRequestEvent(ctx: AssistantContext, prEvent: any, userF
     }
 }
 
-async function handleCommentEvent(ctx: AssistantContext, commentEvent: any) {
+async function handleCommentEvent(ctx: AssistantContext, commentEvent: any): Promise<void> {
     log.info(`Handling comment event from ${commentEvent.comment.user.login}`, {
         event: commentEvent,
         pull_request_ctx: ctx,
     });
 
-    if (commentEvent.comment.user.login === 'vercel[bot]') {
-        await upsertVertesiaComment(ctx, commentEvent);
-        return;
+    if (commentEvent.comment.user.login === 'vercel[bot]' && ctx.deployment?.vercel === undefined) {
+        return upsertVertesiaComment(ctx, commentEvent);
     }
 
     const body = commentEvent.comment.body as string;
     if (!commentEvent.comment.user.login.startsWith('vertesia') && body.toLowerCase().includes('vertesia, please review')) {
-        await startCodeReview(ctx);
-        return;
+        return startCodeReview(ctx);
     }
 
     log.info(`Skip comment event from user: ${commentEvent.comment.user.login}`, { pull_request_ctx: ctx });
-    return;
 }
 
 async function upsertVertesiaComment(ctx: AssistantContext, vercelCommentEvent: any) {
