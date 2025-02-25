936aae2354981c3a83b4524364f36e2c5e3d7144
Mincong Huang <mhuang@vertesiahq.com>
2025-02-25 19:07:05 +0100

    fix

--- apps/github-agent/src/workflows/submodule.ts
+++ apps/github-agent/src/workflows/submodule.ts
@@ -73,7 +73,7 @@ export async function updateSdkSubmodule(request: UpdateSdkSubmoduleRequest): Pr
     await updateGitSubmodule({
         org: "vertesia",
         repo: "studio",
-        branch: newBranch,
+        sha: newBranch,
         path: "composableai",
         submoduleSha: request.commit,
         commitMessage: msg,
